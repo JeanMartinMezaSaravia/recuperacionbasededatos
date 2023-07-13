f32b4f93db90318626bf94d27fef34c1143ef500
Jean Meza Saravia <126886107+JeanMartinMezaSaravia@users.noreply.github.com>
2023-07-13 18:28:09 -0500

    Update JM.sql

--- JM.sql
+++ JM.sql
@@ -1,116 +1,144 @@
-CREATE DATABASE dbElectrodomesticos;
-USE dbElectrodomesticos;
-CREATE TABLE  PROVEEDOR (
-    CODPROV char(5)  NOT NULL COMMENT 'Código de proveedor',
-    RAZSOCPROV varchar(90)  NOT NULL COMMENT 'Razón social del proveedor',
-    RUCPROV varchar(11)  NOT NULL COMMENT 'RUC del proveedor',
-    EMAPROV varchar(70)  NOT NULL COMMENT 'Email del proveedor',
-    ESTPROV char(1)  NOT NULL COMMENT 'Estado del proveedor A(Activo)) I (Inactivo)',
-    CONSTRAINT  PROVEEDOR_pk PRIMARY KEY (CODPROV)
-) COMMENT 'Crear la tabla PROVEEDOR';
-INSERT INTO PROVEEDOR ( CODPROV, RAZSOCPROV, RUCPROV, EMAPROV, ESTPROV )
-values
-('PRV01','LG Corporation' ,'','987485152','Av.Miraflores','A'),
-('PRV02','SONY' ,'','Av.Miraflores','A'),
-('PRV03','SANSUNG' ,'85','Av.Miraflores','A'),
-('PRV04','OSTER SRL' ,'55663214478'Av.Miraflores','A'),
-('PRV05','ASUS' ,'99663325478'Av.Miraflores','A');
+CREATE database dbElectrodomesticos;
+use dbElectrodomesticos;
 
-
-
+-- tables
 -- Table: CLIENTE
 CREATE TABLE CLIENTE (
-    CODCLI char(4)  NOT NULL COMMENT 'Código del cliente',
-    NOMCLI varchar(60)  NOT NULL COMMENT 'Nombre de cliente',
-    APECLI varchar(80)  NOT NULL COMMENT 'Apellido de cliente',
-    EMACLI varchar(80)  NOT NULL COMMENT 'Email del cliente',
-    CELCLI char(9)  NOT NULL COMMENT 'Celular del cliente',
-    DIRCLI varchar(70)  NOT NULL COMMENT 'Direccion del cliente',
-    ESTCLI char(1)  NOT NULL COMMENT 'Estado cliente A(activo) I(inactivo)',
+    CODCLI char(4)  NOT NULL,
+    NOMCLI varchar(60)  NOT NULL,
+    APECLI varchar(80)  NOT NULL,
+    EMACLI varchar(80)  NOT NULL,
+    CELCLI char(9)  NOT NULL,
+    DIRCLI varchar(70)  NOT NULL,
+    ESTCLI char(1)  NOT NULL,
     CONSTRAINT CLIENTE_pk PRIMARY KEY (CODCLI)
-) COMMENT 'Crea la tabla cliente  ';
-INSERT INTO CLIENTE ( CODCLI, NOMCLI,  APECLI, EMACLI, CELCLI, DIRCLI, ESTCLI  )
-values
-('CL01','Juana' ,'Campos Ortìz','juana.campos@gmail.com','987485152','Av.Miraflores','A'),
-('CL02','Sofia' ,'Barrios Salcedo','sofia.barrios@gmail.com','','Av.Miraflores','A'),
-('CL03','Claudio' ,'Pèrez luna','claudio.perez@gmail.com','984512147','Av.Miraflores','A'),
-('CL04','Marco' ,'Àvila Manco ','marco.avila@gmail.com','','Av.Miraflores','A'),
-('CL05','Anastacio' ,'Salomon Inti','anastasio.salomon@gmail.com','','Av.Miraflores','A');
+);
+/**--------------------------------------------------------------------------**/
+INSERT INTO CLIENTE (CODCLI, NOMCLI, APECLI, EMACLI, CELCLI, DIRCLI, ESTCLI)
+VALUES 
+('CL01', 'Juana', 'Campos Ortiz', 'juana.campos@gmail.com', '987485152', 'av. Miraflores', 'A'),
+('CL02', 'Sofia', 'Barrios Salsedo', 'sofia.barios@outlook.com', '', 'jr. Huallaga', 'A'),
+('CL03', 'Claudio', 'Perez Luna', 'claudio.perez@outlook.com', '984512147', 'av. libertadores', 'A'),
+('CL04', 'Marcos', 'Avila Manco', 'marcos.avila@yahoo.com', '', 'Calle huallaga', 'A'),
+('CL05', 'Anastacio', 'Salomon int', 'anastacio.salomon@gamil.com', '', 'jr san martin', 'A');
+ select*from CLIENTE;
+
 -- Table: COMPRA
 CREATE TABLE COMPRA (
-    CODCOM char(5)  NOT NULL COMMENT 'Código de compra',
-    FECCOM datetime  NOT NULL COMMENT 'Fecha en que se realizó la compra',
-    CODVEND char(4)  NOT NULL COMMENT 'Código de Proveedor',
-    CODPROV char(5)  NOT NULL COMMENT 'Código de vendedor',
-    ESTCOM char(1)  NOT NULL COMMENT 'Estado de compra Activo (A) I (Inactivo)',
-    VENDEDOR_CODNED char(4)  NOT NULL,
-    PROVEEDOR_CODPROV char(5)  NOT NULL,
+    CODCOM char(5)  NOT NULL,
+    FECCOM datetime  NOT NULL,
+    CODPROV char(4)  NOT NULL,
+    CODVEND char(5)  NOT NULL,
+    ESTCOM char(1)  NOT NULL,
+    VENDEDOR_CODVEND char(4)  NOT NULL,
+    PROVEDOR_CODPROV char(5)  NOT NULL,
     CONSTRAINT COMPRA_pk PRIMARY KEY (CODCOM)
-) COMMENT 'Crear la tabla COMPRA';
-
+);
 
 -- Table: COMPRADETALLE
 CREATE TABLE COMPRADETALLE (
-    IDCOMDET int  NOT NULL COMMENT 'Identificador de detalle de compra (auto incrementable)',
-    CODCOM char(5)  NOT NULL COMMENT 'Código de venta',
-    CODPRO char(5)  NOT NULL COMMENT 'Código de producto',
-    CANTPRO int  NOT NULL COMMENT 'Cantidad de productos comprados',
-    PRODUCTO_CODPRO char(5)  NOT NULL,
+    IDCOMDET int  NOT NULL,
+    CODCOM char(5)  NOT NULL,
+    CODPRO char(5)  NOT NULL,
+    CANTPRO int  NOT NULL,
     COMPRA_CODCOM char(5)  NOT NULL,
+    PRODUCTO_CODPRO char(5)  NOT NULL,
     CONSTRAINT COMPRADETALLE_pk PRIMARY KEY (IDCOMDET)
-) COMMENT 'Crear la tabla COMPRADETALLE';
+);
 
 -- Table: PRODUCTO
 CREATE TABLE PRODUCTO (
-    CODPRO char(5)  NOT NULL COMMENT 'Código de producto',
-    DESCPRO varchar(80)  NOT NULL COMMENT 'Descripción del producto',
-    CATPRO char(1)  NOT NULL COMMENT 'Categorías de Producto: Refrigeración (1), Televisores (2), Computadoras (3) y  Teléfono (4)',
-    PREPRO decimal(8,2)  NOT NULL COMMENT 'Precio del producto',
-    STOCKPRO int  NOT NULL COMMENT 'Stock de producto',
-    ESTPRO char(1)  NOT NULL COMMENT 'Estado de producto Activo (A) - I (Inactivo)',
+    CODPRO char(5)  NOT NULL,
+    DESCPRO varchar(80)  NOT NULL,
+    CATPRO char(1)  NOT NULL,
+    PREPRO decimal(8,2)  NOT NULL,
+    STOCKPRO int  NOT NULL,
+    ESTPRO char(1)  NOT NULL,
     CONSTRAINT PRODUCTO_pk PRIMARY KEY (CODPRO)
-) COMMENT 'Crear la tabla PRODUCTO';
+);
+/**--------------------------------------------------------------------------**/
+INSERT INTO PRODUCTO (CODPRO, DESCPRO, CATPRO, PREPRO, STOCKPRO, ESTPRO)
+VALUES 
+('PRO01', 'Refrigeradora LG Side Bv Side', '1', 4149, 15, 'A'),
+('PRO02', 'Refrigeradora SBS 6021', '1', 3599, 10, 'A'),
+('PRO03', 'Refrigeradora Top Mount 500 L', '1', 2799, 8, 'A'),
+('PRO04', 'TV SAMSUNG UHD 55', '2', 1799, 25, 'A'),
+('PRO05', 'Televisor 65" LG UHD 4K', '2', 2999, 20, 'A'),
+('PRO06', 'TV CRYSTAL UHD 55', '2', 2299, 7, 'A'),
+('PRO07', 'ASUS X415JA Core i3 10a Gen 14" 3', '3', 1099, 17, 'A'),
+('PRO08', 'Lenovo IdeaPad 5i Intel Core i7 14"', '3', 3099, 10, 'A'),
+('PRO09', 'Laptop HP 15-dw1085la Intel Core i3', '3', 1600, 15, 'A'),
+('PRO10', 'Galaxy A52 128GB', '4', 1200, 25, 'A'),
+('PRO11', 'iPhone 11 64GB Morado', '4', 2600, 30, 'A'),
+('PRO12', 'POCO X3 GT 5G 256GB &GR', '4', 1450, 20, 'A');
+
+
+
+
+-- Table: PROVEDOR
+CREATE TABLE PROVEDOR (
+    CODPROV char(5)  NOT NULL,
+    RAZSOCPROV varchar(90)  NOT NULL,
+    RUCPROV varchar(11)  NOT NULL,
+    EMAPROV varchar(70)  NOT NULL,
+    ESTPROV char(1)  NOT NULL,
+    CONSTRAINT PROVEDOR_pk PRIMARY KEY (CODPROV)
+);
+/**--------------------------------------------------------------------------**/
+INSERT INTO PROVEDOR (CODPROV, RAZSOCPROV, RUCPROV, EMAPROV, ESTPROV)
+VALUES 
+('PRV01', 'LG Corporation', '87542136951', 'informes@lg.com.pe', 'A'),
+('PRV02', 'SONY', '45213698741', 'informes@sony.com.pe', 'A'),
+('PRV03', 'SAMSUNG', '85321697661', 'informes@samsung.com.pe', 'A'),
+('PRV04', 'OSTER SRL', '55663214478', 'informes@oster.com.pe', 'A'),
+('PRV05', 'ASUS', '99663325478', 'informes@asus.com.pe', 'A');
+
+
+
+
 
 -- Table: VENDEDOR
 CREATE TABLE VENDEDOR (
-    CODNED char(4)  NOT NULL COMMENT 'Código del vendedor',
-    NOMVEND varchar(60)  NOT NULL COMMENT 'Nombre del vendedor',
-    APEVEND varchar(80)  NOT NULL COMMENT 'Apellidos del vendedor',
-    EMAVEND varchar(80)  NOT NULL COMMENT 'Email del vendedor',
-    CELVEND char(9)  NOT NULL COMMENT 'Celular del vendedor',
-    DIRVEND varchar(70)  NOT NULL COMMENT 'Direccion deL vendedor',
-    ESTVEND char(1)  NOT NULL COMMENT 'Estado vendedor A(activo) I(inactivo)',
-    CONSTRAINT VENDEDOR_pk PRIMARY KEY (CODNED)
-) COMMENT 'Crea la tabla vendedor';
+    CODVEND char(4)  NOT NULL,
+    NOMVEND varchar(60)  NOT NULL,
+    APEVEND varchar(80)  NOT NULL,
+    EMAVEND varchar(80)  NOT NULL,
+    CELVEND char(9)  NOT NULL,
+    DIRVEND varchar(70)  NOT NULL,
+    ESTVEND char(1)  NOT NULL,
+    CONSTRAINT VENDEDOR_pk PRIMARY KEY (CODVEND)
+);
+/**--------------------------------------------------------------------------**/
+INSERT INTO VENDEDOR (CODVEND, NOMVEND, APEVEND, EMAVEND, CELVEND, DIRVEND, ESTVEND)
+VALUES 
+('VEN1', 'Gloria', 'Carriales Paredes', 'gloria.carriales@gmail.com', '984574123', 'calle las begonias', 'A'),
+('VEN2', 'Gabriel', 'Lozada Lombardi', 'gabriel.lozada@gmail.com', '974512368', 'av. los girasoles', 'A'),
+('VEN3', 'Gilberto', 'Martinez Guerra', 'gilberto.martinez@gmail.com', '', 'jr. palomares', 'A');
 
-INSERT INTO VENDEDOR ( CODNED, NOMVEND,  APEVEND, EMAVEND, CELVEND, DIRVEND, ESTVEND  )
-values
-('VEN1','Gloria' ,'Carrizales Paredes','Gloria.carrizales@gmail.com','984574123','Calle Las Begonias','A'),
-('VEN2','Gabriel' ,'Lozada Lombardi','gabriel.lozada@gmail.com','974512368','Av.Los Girasoles','A'),
-('VEN3','Gilberto' ,'Martinez Guerra','gilberto.martinez@gmail.com','','Jr.Palomares','A');
+
 
 -- Table: VENTA
 CREATE TABLE VENTA (
-    CODVEN char(5)  NOT NULL COMMENT 'Código de venta',
-    FECVEN datetime  NOT NULL COMMENT 'Fecha en que se realizó la venta',
-    CODCLI char(4)  NOT NULL COMMENT 'Código del cliente',
-    CODVEND char(4)  NOT NULL COMMENT 'Código del vendedor',
-    ESTVEN char(1)  NOT NULL COMMENT 'Estado de venta A(Activo) (I) Inactivo',
-    VENDEDOR_CODNED char(4)  NOT NULL,
+    CODVEN char(5)  NOT NULL,
+    FECVEN datetime  NOT NULL,
+    CODCLI char(4)  NOT NULL,
+    CODVEND char(4)  NOT NULL,
+    ESTVEN char(1)  NOT NULL,
+    VENDEDOR_CODVEND char(4)  NOT NULL,
     CLIENTE_CODCLI char(4)  NOT NULL,
     CONSTRAINT VENTA_pk PRIMARY KEY (CODVEN)
-) COMMENT 'Crear la tabla VENTA';
+);
 
 -- Table: VENTADETALLE
 CREATE TABLE VENTADETALLE (
-    IDVENEDT int  NOT NULL COMMENT 'Identificador de detalle de venta (auto incrementable)',
-    CODVEN char(5)  NOT NULL COMMENT 'Código de venta',
-    CODPRO char(5)  NOT NULL COMMENT 'Código de producto',
-    CANTPRO int  NOT NULL COMMENT 'Cantidad de productos a vender',
+    IDVENDET int  NOT NULL,
+    CODVEN char(5)  NOT NULL,
+    CODPRO char(5)  NOT NULL,
+    CANTPRO int  NOT NULL,
     VENTA_CODVEN char(5)  NOT NULL,
     PRODUCTO_CODPRO char(5)  NOT NULL,
-    CONSTRAINT VENTADETALLE_pk PRIMARY KEY (IDVENEDT)
-) COMMENT 'Crear la tabla VENTADETALLE';
+    CONSTRAINT VENTADETALLE_pk PRIMARY KEY (IDVENDET)
+);
 
 -- foreign keys
 -- Reference: COMPRADETALLE_COMPRA (table: COMPRADETALLE)
@@ -121,13 +149,13 @@ ALTER TABLE COMPRADETALLE ADD CONSTRAINT COMPRADETALLE_COMPRA FOREIGN KEY COMPRA
 ALTER TABLE COMPRADETALLE ADD CONSTRAINT COMPRADETALLE_PRODUCTO FOREIGN KEY COMPRADETALLE_PRODUCTO (PRODUCTO_CODPRO)
     REFERENCES PRODUCTO (CODPRO);
 
--- Reference: COMPRA_ PROVEEDOR (table: COMPRA)
-ALTER TABLE COMPRA ADD CONSTRAINT COMPRA_PROVEEDOR FOREIGN KEY (PROVEEDOR_CODPROV)
-REFERENCES PROVEEDOR (CODPROV);
+-- Reference: COMPRA_PROVEDOR (table: COMPRA)
+ALTER TABLE COMPRA ADD CONSTRAINT COMPRA_PROVEDOR FOREIGN KEY COMPRA_PROVEDOR (PROVEDOR_CODPROV)
+    REFERENCES PROVEDOR (CODPROV);
 
 -- Reference: COMPRA_VENDEDOR (table: COMPRA)
-ALTER TABLE COMPRA ADD CONSTRAINT COMPRA_VENDEDOR FOREIGN KEY COMPRA_VENDEDOR (VENDEDOR_CODNED)
-    REFERENCES VENDEDOR (CODNED);
+ALTER TABLE COMPRA ADD CONSTRAINT COMPRA_VENDEDOR FOREIGN KEY COMPRA_VENDEDOR (VENDEDOR_CODVEND)
+    REFERENCES VENDEDOR (CODVEND);
 
 -- Reference: VENTADETALLE_PRODUCTO (table: VENTADETALLE)
 ALTER TABLE VENTADETALLE ADD CONSTRAINT VENTADETALLE_PRODUCTO FOREIGN KEY VENTADETALLE_PRODUCTO (PRODUCTO_CODPRO)
@@ -142,8 +170,8 @@ ALTER TABLE VENTA ADD CONSTRAINT VENTA_CLIENTE FOREIGN KEY VENTA_CLIENTE (CLIENT
     REFERENCES CLIENTE (CODCLI);
 
 -- Reference: VENTA_VENDEDOR (table: VENTA)
-ALTER TABLE VENTA ADD CONSTRAINT VENTA_VENDEDOR FOREIGN KEY VENTA_VENDEDOR (VENDEDOR_CODNED)
-    REFERENCES VENDEDOR (CODNED);
+ALTER TABLE VENTA ADD CONSTRAINT VENTA_VENDEDOR FOREIGN KEY VENTA_VENDEDOR (VENDEDOR_CODVEND)
+    REFERENCES VENDEDOR (CODVEND);
 
 -- End of file.
 
